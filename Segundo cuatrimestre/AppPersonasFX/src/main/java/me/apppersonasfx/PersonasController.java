6ad04d070e32028ce0f3aa2fb6af7532e2404283
COMPU-S026\daw1 <nanacaram06@gmail.com>
2026-04-23 09:49:45 +0200

    a

--- Segundo cuatrimestre/AppPersonasFX/src/main/java/me/apppersonasfx/PersonasController.java
+++ Segundo cuatrimestre/AppPersonasFX/src/main/java/me/apppersonasfx/PersonasController.java
@@ -1,9 +1,20 @@
 package me.apppersonasfx;
 
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import me.apppersonasfx.model.Persona;
+
 
 public class PersonasController {
+
+    private Persona pp;
+
+    private ObservableList<Persona> personas = FXCollections.observableArrayList();
+
+
     @FXML
     private Label welcomeText;
 
